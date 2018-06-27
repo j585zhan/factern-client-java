285ace12a7632357a3310a1cccc90f61d9261823
Karthik Velakur <kvelakur@finovertech.com>
2018-06-27 10:04:53 -0400

    Sign only before deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+mvn veryify -P sign-artifacts --settings settings.xml
+
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     mvn deploy -P sign-artifacts --settings settings.xml
 fi
