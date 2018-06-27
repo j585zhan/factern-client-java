4571de8e16b3625672cfda8300bbeaf328b3444c
Karthik Velakur <kvelakur@finovertech.com>
2018-06-27 09:59:05 -0400

    Artifact signing test

--- before-deploy.sh
+++ before-deploy.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_d16f88065f17_key -iv $encrypted_d16f88065f17_iv -in codesigning.asc.enc -out codesigning.asc -d
-    gpg --fast-import signingkey.asc
-fi
+
+openssl aes-256-cbc -K $encrypted_d16f88065f17_key -iv $encrypted_d16f88065f17_iv -in codesigning.asc.enc -out codesigning.asc -d
+gpg --fast-import signingkey.asc
